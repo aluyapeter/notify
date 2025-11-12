81c079376ad9b3dae9644d3a00a164e6c3ba39b1
Ehiz-js <azamegbesamuel@gmail.com>
2025-11-12 04:44:07 +0100

    Complete initial project skeleton

---
+++ template-service/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.TEMPLATE_SERVICE_PORT ?? 8002);
+}
+bootstrap();
